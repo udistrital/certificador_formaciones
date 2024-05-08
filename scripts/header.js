084d0d1e640a4854691b3187fdebc5183f47f670
jeassons <jeassonsuarez@outlook.es>
2024-05-08 12:36:06 -0500

    agregando funcion de open sesion controles2

--- scripts/header.js
+++ scripts/header.js
@@ -1,32 +1,33 @@
-const $navlista = document.getElementById("nav-lista");
-const $btnHamNav = document.getElementById("nav-menu-hamburguer");
+// const $navlista = document.getElementById("nav-lista");
+// const $btnHamNav = document.getElementById("nav-menu-hamburguer");
 
-$btnHamNav.addEventListener("click", (e) => {
-  $navlista.classList.toggle("nav-lista-disable");
-});
+// $btnHamNav.addEventListener("click", (e) => {
+//   $navlista.classList.toggle("nav-lista-disable");
+// });
 
-const $headerSesionBtn = document.getElementById("sesion-div-controles");
-const $itemSesion = document.getElementById("nav-item-sublista-sesion");
+const $headerSesionBtn = document.querySelector(".sesion__controles");
+console.log($headerSesionBtn);
+const $itemSesion = document.querySelector(".sesion__acciones");
 
 $headerSesionBtn.addEventListener("click", (e) => {
-  $itemSesion.classList.toggle("nav-item-sublista-disable");
+  $itemSesion.classList.toggle("sesion__acciones--disabled");
 });
 
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".nli-tf")) {
-    document.getElementById("nis-tf").classList.toggle("nis-tf");
-  } else if (e.target.matches(".nli-f")) {
-    document.getElementById("nis-f").classList.toggle("nis-f");
-  }
-});
+// document.addEventListener("click", (e) => {
+//   if (e.target.matches(".nli-tf")) {
+//     document.getElementById("nis-tf").classList.toggle("nis-tf");
+//   } else if (e.target.matches(".nli-f")) {
+//     document.getElementById("nis-f").classList.toggle("nis-f");
+//   }
+// });
 
-const identificarUsuario = async () => {
-  let data = await JSON.parse(sessionStorage.getItem("data"));
-  document.getElementById("sesion-nombreu").textContent =
-    data.nombre + " " + data.apellido;
-  document.getElementById("sesion-rol").textContent = data.rol;
-  document.getElementById("sesion-img-icon").textContent =
-    data.nombre[0] + data.apellido[0];
-};
+// const identificarUsuario = async () => {
+//   let data = await JSON.parse(sessionStorage.getItem("data"));
+//   document.getElementById("sesion-nombreu").textContent =
+//     data.nombre + " " + data.apellido;
+//   document.getElementById("sesion-rol").textContent = data.rol;
+//   document.getElementById("sesion-img-icon").textContent =
+//     data.nombre[0] + data.apellido[0];
+// };
 
-identificarUsuario();
+// identificarUsuario();
