d11a9002654cf3d51f67f095803a80542b7028c4
jeassons <jeassonsuarez@outlook.es>
2024-05-08 20:58:24 -0500

    agregando navegacion, menu hamburger, y responsive

--- scripts/header.js
+++ scripts/header.js
@@ -6,12 +6,19 @@
 // });
 
 const $headerSesionBtn = document.querySelector(".sesion__controles");
-console.log($headerSesionBtn);
 const $itemSesion = document.querySelector(".sesion__acciones");
 
 $headerSesionBtn.addEventListener("click", (e) => {
   $itemSesion.classList.toggle("sesion__acciones--disabled");
 });
+
+const $btn_ham = document.querySelector(".nav__menu");
+console.log($btn_ham);
+
+$btn_ham.addEventListener("click", (e)=>{
+  document.querySelector(".nav").classList.toggle("nav--disabled")
+})
+
 
 // document.addEventListener("click", (e) => {
 //   if (e.target.matches(".nli-tf")) {
@@ -21,13 +28,13 @@ $headerSesionBtn.addEventListener("click", (e) => {
 //   }
 // });
 
-// const identificarUsuario = async () => {
-//   let data = await JSON.parse(sessionStorage.getItem("data"));
-//   document.getElementById("sesion-nombreu").textContent =
-//     data.nombre + " " + data.apellido;
-//   document.getElementById("sesion-rol").textContent = data.rol;
-//   document.getElementById("sesion-img-icon").textContent =
-//     data.nombre[0] + data.apellido[0];
-// };
+const identificarUsuario = async () => {
+  let data = await JSON.parse(sessionStorage.getItem("data"));
+  document.querySelector(".texto_nombreusuario").textContent =
+    data.nombre + " " + data.apellido;
+  document.querySelector(".texto_rol").textContent = data.rol;
+  document.querySelector(".controles__icono").textContent =
+    data.nombre[0] + data.apellido[0];
+};
 
-// identificarUsuario();
+identificarUsuario();
