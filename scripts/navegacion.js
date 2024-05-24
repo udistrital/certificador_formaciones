a22c8928a7e29fa959860560ac7af38680611a7f
jeassons <jeassonsuarez@outlook.es>
2024-05-24 16:36:32 -0500

    mejorando, agregacion de nombre de la formacion a la navegacion

--- scripts/navegacion.js
+++ scripts/navegacion.js
@@ -23,6 +23,16 @@ export function registrarNavegacion() {
 // Llamada a la función para registrar la navegación en la primera carga
 registrarNavegacion();
 
+const obtenerInformacionURL = (link) => {
+  let informacion = new URLSearchParams(link);
+
+  let paramsObj = {};
+  informacion.forEach((value, key) => {
+    paramsObj[key] = value;
+  });
+  console.log(paramsObj);
+  return paramsObj;
+};
 const llenarHistorial = () => {
   const $fargmento = document.createDocumentFragment(),
     $template = document.getElementById("template-historial-enlace").content;
@@ -33,59 +43,60 @@ const llenarHistorial = () => {
   // console.log($artHistorial);
   console.log(linksVisitados);
   linksVisitados.forEach((link) => {
+    let nombreFormacion = obtenerInformacionURL(link).nombreFormacion;
     $template.querySelector("a").setAttribute("href", link);
     $template.querySelector("a").setAttribute("target", "_blank");
     $template.querySelector("a").textContent = `/${
       link.includes("index.html")
         ? "home"
-        : link.includes("CursoConTutorPage")
-        ? "Curso con tutor"
-        : link.includes("CursoConTutorCohortesPage")
-        ? "Cohortes curso con tutor"
-        : link.includes("AsistenciasPage")
-        ? "Asistencia"
-        : link.includes("CertificadosEmitidosPage")
-        ? "Certificados emitidos"
-        : link.includes("GenerarCertificadosPage")
-        ? "Generar certificado"
-        : link.includes("CursoMoocPage")
-        ? "Curso Mooc"
-        : link.includes("CapacitacionTaller")
-        ? "Capacitacion o taller"
-        : link.includes("DiplomadosPage")
-        ? "Diplomado"
-        : link.includes("AsistenciasPage")
-        ? "Asistencia"
-        : link.includes("CertificadosEmitidosPage")
-        ? "Certificados emitidos"
-        : link.includes("GenerarCertificadosPage")
-        ? "Generar certificados"
-        : link.includes("CursoConTutorCohortesPage")
-        ? "Cohortes curso con tutor"
-        : link.includes("CursoMoocCohortesPage")
-        ? "Cohortes curso Mooc"
-        : link.includes("CapacitacionCohortesPage")
-        ? "Cohortes Capacitacion o taller"
-        : link.includes("DiplomadoCohortesPage")
-        ? "Cohortes diplomados"
-        : link.includes("CursosModulos")
-        ? "Modulos diplomados"
-        : link.includes("EventosCohortesPage")
-        ? "Cohortes eventos"
-        : link.includes("EventosCohortesPage")
-        ? "Eventos"
-        : link.includes("PageFormsAsistencias")
-        ? "Formularios asistencias"
-        : link.includes("PageFormsDocumentacion")
-        ? "Formularios documentación"
-        : link.includes("PageFormsInscripciones")
-        ? "Formularios de inscripción"
-        : link.includes("PageFormsPostulaciones")
-        ? "Formularios de inscripcion ponentes"
-        : link.includes("PageFormsEvidencias")
-        ? "Formularios de evidencias"
-        : link.includes("EventosPage")
-        ? "Eventos"
+        : link.includes("CursoConTutorPage") 
+        ? `Curso con tutor ${nombreFormacion ? nombreFormacion : ""}` 
+        : link.includes("CursoConTutorCohortesPage") 
+        ? `Cohortes curso con tutor ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("AsistenciasPage") 
+        ? `Asistencia ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CertificadosEmitidosPage")  
+        ? `Certificados emitidos ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("GenerarCertificadosPage")  
+        ? `Generar certificado ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CursoMoocPage")  
+        ? `Curso Mooc ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CapacitacionTaller")  
+        ? `Capacitacion o taller ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("DiplomadosPage")  
+        ? `Diplomado ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("AsistenciasPage")  
+        ? `Asistencia ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CertificadosEmitidosPage")  
+        ? `Certificados emitidos ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("GenerarCertificadosPage")  
+        ? `Generar certificados ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CursoConTutorCohortesPage")  
+        ? `Cohortes curso con tutor ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CursoMoocCohortesPage") 
+        ? `Cohortes curso Mooc ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CapacitacionCohortesPage")  
+        ? `Cohortes Capacitacion o taller ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("DiplomadoCohortesPage")  
+        ? `Cohortes diplomados ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("CursosModulos")  
+        ? `Modulos diplomados ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("EventosCohortesPage") 
+        ? `Cohortes eventos ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("EventosCohortesPage")  
+        ? `Eventos ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("PageFormsAsistencias")  
+        ? `Formularios asistencias ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("PageFormsDocumentacion")  
+        ? `Formularios documentación ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("PageFormsInscripciones")  
+        ? `Formularios de inscripción ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("PageFormsPostulaciones")  
+        ? `Formularios de inscripcion ponentes ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("PageFormsEvidencias")  
+        ? `Formularios de evidencias ${nombreFormacion ? nombreFormacion : ""}`
+        : link.includes("EventosPage")  
+        ? `Eventos ${nombreFormacion ? nombreFormacion : ""}`
         : link.includes()
         ? ""
         : ""
